2d15e4955943d946927a0016338770e297619cbd
Thalison Silva <contato.thalison@outlook.com>
2023-05-21 20:02:07 -0300

    Curso de golang - pacotes

--- golang/codigo/Aula 02 - pacotes/pacotes/main.go
+++ golang/codigo/Aula 02 - pacotes/pacotes/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"modulo/auxiliar"
 	"fmt" 
+	"github.com/badoux/checkmail"
 ) 
 	// Por padrão no go Toda função quer for exportada - deve ter um comentario o que ela faz!
 func main() { 
 	fmt.Println("Hello World")
 	auxiliar.Escrever()
+	
+	erro := checkmail.ValidateFormat("devthalison@gmail.com")
+	fmt.Println(erro)
 }
