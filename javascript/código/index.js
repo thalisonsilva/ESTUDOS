cfd8b12c4f41183c75672868ac5ff6938b10d09f
Thalison Silva <contato.thalison@outlook.com>
2023-06-10 19:19:28 -0300

    javascript

---
+++ javascript/código/index.js
@@ -0,0 +1,14 @@
+function verifica() {
+    var nome = "Bruno";
+    var senha = "b1234";
+
+    var nomeInput = document.getElementById("nomeInput").value;
+    var senhaInput = document.getElementById("senhaInput").value;
+
+    if (nomeInput === nome && senhaInput === senha) {
+      alert("Bem-vindo!");
+    } else {
+      alert("Acesso negado.");
+    }
+  }
+
