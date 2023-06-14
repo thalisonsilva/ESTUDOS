98a555a2ad917b58776c2ad347ba9ece701eed64
Thalison Silva <contato.thalison@outlook.com>
2023-06-14 20:18:57 -0300

    javascript aula 03

---
+++ javascript/código/aula 03/aula03.js
@@ -0,0 +1,11 @@
+// Podemos utilizar ('', "", ``) para exibir textos no javascript!!
+
+
+console.log("Olá mundo"); // o ponto e virgula e opcional no javascript! 
+console.log('Thalison');  //String = texto. 
+console.log(`Thalison moreira`); 
+
+
+//Exibir número em javascript
+
+console.log(123424, 17.85) // usamos o  (.) para número fluantes!! 
