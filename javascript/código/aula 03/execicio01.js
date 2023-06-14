0729c300e33d1292e312bfefef80c469e12a6789
Thalison Silva <contato.thalison@outlook.com>
2023-06-14 20:45:11 -0300

    javascript ex002

--- javascript/código/aula 03/execicio01.js
+++ javascript/código/aula 03/execicio01.js
@@ -1,1 +1,6 @@
 console.log('Meu nome é "Thalison". Estou aprendendo Javascript às', 20, 'da noite')
+
+
+//Ex002
+
+console.log('Sou Developer fullstack "Meu nome é Thalison". Tenho 25 anos e moro em Goiânia')
