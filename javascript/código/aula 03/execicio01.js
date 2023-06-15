33c5bd8dca30de6214edcd5c0b09996a2c390011
Thalison Silva <contato.thalison@outlook.com>
2023-06-14 21:03:03 -0300

    javascript ex003

--- javascript/código/aula 03/execicio01.js
+++ javascript/código/aula 03/execicio01.js
@@ -4,3 +4,11 @@ console.log('Meu nome é "Thalison". Estou aprendendo Javascript às', 20, 'da n
 //Ex002
 
 console.log('Sou Developer fullstack "Meu nome é Thalison". Tenho 25 anos e moro em Goiânia')
+
+//Ex003
+
+let nome = "Thalison"
+let idade = 25
+let ano = 1998
+
+console.log(nome, idade, ano)
