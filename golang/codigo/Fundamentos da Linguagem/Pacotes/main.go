dd94308f415a7d7739b40c7ed680435b76fe36fd
Thalison Silva <contato.thalison@outlook.com>
2023-07-17 21:16:11 -0300

    coment - pckt go

--- golang/codigo/Fundamentos da Linguagem/Pacotes/main.go
+++ golang/codigo/Fundamentos da Linguagem/Pacotes/main.go
@@ -8,6 +8,11 @@ import (
 // Toda função que será exportada, dever ter um comntario em cima!
 func main() {
 	fmt.Println("Olá thalison")
-	auxiliar.Escrever()
+	auxiliar.Escrever()  //Para cria mais de um pacote devemos cria mais de uma pastas.
 	auxiliar.Escrever2()
 }
+
+
+// Quando estamos lidando com mais de um pacotes no GO devemos cria modulos para compilar! 
+// comando: "go mod init modulo" -> você vai centralizar as depedenciar e pacotes.
+// comando: "go build" -> você cria um "modulo.exe" um arquivo binario executavel.
